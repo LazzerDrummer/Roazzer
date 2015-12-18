7001ef9ae1222ae9ea4ea9cc25924b10f6918a1a
Lazzer <lazerdrummer@gmail.com>
2015-12-18 20:15:11 +0200

    Now added road that magically moves.

--- src/home/lazzer/roazzer/Main.java
+++ src/home/lazzer/roazzer/Main.java
@@ -2,16 +2,19 @@ package home.lazzer.roazzer;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame window = new JFrame();
+		JFrame window = new JFrame("ROAZZZZZZZER");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Road myCanvas = new Road();
 		window.getContentPane().add(myCanvas, BorderLayout.CENTER);
 		window.pack();
 		window.setVisible(true);
+		Timer timer = new Timer(25, myCanvas);
+		timer.start();
 	}
 
 }
--- src/home/lazzer/roazzer/Road.java
+++ src/home/lazzer/roazzer/Road.java
@@ -1,23 +1,26 @@
 package home.lazzer.roazzer;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JComponent;
 
-public class Road extends JComponent {
+public class Road extends JComponent implements ActionListener {
 	private static final long serialVersionUID = 7684382000314660093L;
 	
-	int width=300;
-	int height=500;
-	int roadWidth = 150;
+	int width=1500;
+	int height=900;
+	int roadWidth = 200;
 	int lineHeight=15;
+	int time=0;
 	public void paint(Graphics g) {
-		int [] b = {1,2,4,6,12,24,48,24,12,6,4,2,1,7,6,5,4,3,2,1};
+		int b[] = TerrainData.borders;
 		int border = (width-roadWidth)/2;
-	    for(int i=0;i<b.length-1;i++)
+	    for(int i=0;i<height/lineHeight;i++)
 	    {
-	    	int x1 = b[i]+border;
-	    	int x2 = b[i+1]+border;
+	    	int x1 = b[(i+time)%b.length]+border;
+	    	int x2 = b[(i+1+time)%b.length]+border;
 			g.drawLine(x1,height-i*lineHeight,x1,height-(i+1)*lineHeight);
 		    g.drawLine(x1+roadWidth,height-i*lineHeight,x1+roadWidth,height-(i+1)*lineHeight);
 		    g.drawLine(x1,height-(i+1)*lineHeight ,x2,height-(i+1)*lineHeight);
@@ -29,5 +32,9 @@ public class Road extends JComponent {
 		// TODO Auto-generated method stub
 		return new Dimension(width, height);
 	}
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		time++;
+		repaint();
+	}
 }
